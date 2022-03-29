053a8a0ec3fe3a0c653e421aba7e67536304cec4
Jiaqi Chen <200477892@student.georgianc.on.ca>
2022-03-28 20:12:01 -0400

    Add the view and controller files for the movie details.

---
+++ src/main/java/com/example/w22comp1011gcw10/MovieDetailsController.java
@@ -0,0 +1,37 @@
+package com.example.w22comp1011gcw10;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+public class MovieDetailsController {
+
+    @FXML
+    private ListView<?> actorsListView;
+
+    @FXML
+    private Label genreLabel;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label languageLabel;
+
+    @FXML
+    private Label movieTitleLabel;
+
+    @FXML
+    private ListView<?> ratingsListView;
+
+    @FXML
+    private Label releaseDateLabel;
+
+    @FXML
+    private Label runTimeLabel;
+
+    @FXML
+    private Label writtersLabel;
+
+}
